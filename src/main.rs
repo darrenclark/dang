400a95b56922a6386b8b33f85c122411e3dae37a
Darren Clark <darrenjoelclark@icloud.com>
2024-06-21 20:07:10 -0400

    add repl

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,41 @@
-fn main() {
-    let code = "add(1, 2)";
+use rustyline::error::ReadlineError;
+use rustyline::{DefaultEditor, Result};
+
+fn main() -> Result<()> {
+    let mut rl = DefaultEditor::new()?;
+    //if rl.load_history("history.txt").is_err() {
+    //    println!("No previous history.");
+    //}
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                //rl.add_history_entry(line.as_str());
+                print_tree(line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    //rl.save_history("history.txt");
+    Ok(())
+}
+
+fn print_tree(line: String) {
     let mut parser = tree_sitter::Parser::new();
     parser
         .set_language(tree_sitter_dang::language())
         .expect("Error loading dang grammar");
-    let tree = parser.parse(code, None).unwrap();
-    println!("{:?}", tree);
+    let tree = parser.parse(line, None).unwrap();
+    println!("{}", tree.root_node().to_sexp());
 }
